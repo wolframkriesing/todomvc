741a46764b0d3793842a574a45dd0a6c5351a8e0
Wolfram Kriesing <wk@uxebu.com>
2015-01-08 10:08:50 +0100

    Pass a param, better than use of a globla.

--- examples/react/js/main.jsx
+++ examples/react/js/main.jsx
@@ -10,17 +10,18 @@ var app = app || {};
 
 	var model = new app.TodoModel('react-todos');
 
-	var todoApp;
 	function render() {
-		todoApp = React.render(
-			<app.TodoApp model={model}/>,
+		var todoApp = React.render(
+			<app.TodoApp
+				model={model}
+			/>,
 			document.getElementById('todoapp')
 		);
-		startRouter();
+		startRouter(todoApp);
 	}
 
-	function startRouter() {
-		var router = Router({
+	function startRouter(todoApp) {
+		var router = new Router({
 			'/': todoApp.setState.bind(todoApp, {nowShowing: app.ALL_TODOS}),
 			'/active': todoApp.setState.bind(todoApp, {nowShowing: app.ACTIVE_TODOS}),
 			'/completed': todoApp.setState.bind(todoApp, {nowShowing: app.COMPLETED_TODOS})
