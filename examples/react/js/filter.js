4ec3cce0283b5dead432445b500441fe735bab3a
Wolfram Kriesing <wk@uxebu.com>
2015-01-08 18:20:42 +0100

    ES5ify code, so it runs in FF.

--- examples/react/js/filter.js
+++ examples/react/js/filter.js
@@ -8,31 +8,34 @@ var app = app || {};
 (function () {
   'use strict';
 
-  class Filter {
-    constructor(filters) {
-      this._filters = filters;
-      this._current = 0;
-    }
+  //class Filter {
+  //  constructor(filters) {
+  function Filter(filters) {
+    this._filters = filters;
+    this._current = 0;
+  }
 
-    setCurrent(index) {
+  Filter.prototype = {
+
+    setCurrent: function(index) {
       this._current = index;
-    }
+    },
 
-    applyOn(todos) {
+    applyOn: function(todos) {
       var criteria = this._filters[this._current].criteria;
       if (criteria) {
         return todos.filter(criteria);
       }
       return todos;
-    }
+    },
 
-    getViewValues() {
+    getViewValues: function() {
       var currentIndex = this._current;
       return this._filters.map(function(by, idx) {
         return {url: '#' + by.url, linkText: by.linkText, selected: idx === currentIndex};
       });
     }
-  }
+  };
 
   app.Filter = Filter;
 })();
